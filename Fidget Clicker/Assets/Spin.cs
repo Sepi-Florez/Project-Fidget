93510ac55991129ec3bcc47cc514332eca1218db
David Vink <david.vink@hotmail.com>
2017-05-18 17:39:55 +0200

    Added project

---
+++ Fidget Clicker/Assets/Spin.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin : MonoBehaviour {
+    public float force;
+    public float deceleration;
+
+    public float lerpTime;
+    public float currentLerpTime;
+
+    void Update() {
+        if (Input.GetButtonDown("Jump")) {
+            currentLerpTime = 0;
+            force += 10;
+
+        }
+        currentLerpTime += Time.deltaTime;
+        if(currentLerpTime > lerpTime) {
+            currentLerpTime = lerpTime;
+        }
+        float t = currentLerpTime / lerpTime;
+        t = Mathf.Sin(t * Mathf.PI * 0.5f);
+        force = Mathf.Lerp(force, 0, t);
+        transform.Rotate(0, 0, force);
+        if(force <= 0.1) {
+            force = 0;
+        }
+    }
+}
